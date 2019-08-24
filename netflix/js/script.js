cdbd30bd2620d01607d778d7220e35e01380d057
Kachanov2508 <kachanov2508@yandex.ru>
2019-08-24 23:45:30 +0300

    ih

---
+++ netflix/js/script.js
@@ -0,0 +1,22 @@
+const tabItem = document.querySelectorAll('.tab-item');
+const tabContent = document.querySelectorAll('.tab-content-item');
+
+tabItem.forEach(item => item.addEventListener('click', show));
+
+
+function show() {
+    hideBorder();
+    this.classList.add('tab-border');
+
+    hideContent();
+    const tabContent = `#${this.id}-content`;
+    document.querySelector(tabContent).classList.add('show');
+}
+
+function hideBorder() {
+    tabItem.forEach(item => { item.classList.remove('tab-border') });
+}
+
+function hideContent() {
+    tabContent.forEach(item => { item.classList.remove('show') });
+}
