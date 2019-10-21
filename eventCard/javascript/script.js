3b8211358ddbdf161aef8f5880272d7a3720e477
Kachanov2508 <Kachanov2508@yandex.ru>
2019-10-21 12:52:42 +0300

    ok

--- eventCard/javascript/script.js
+++ eventCard/javascript/script.js
@@ -22,3 +22,32 @@ function cardOpen() {
     cardRightLogo.classList.toggle('card-right-logo-open');
     // cardPrice.classList.toggle('card-price-open');
 }
+
+
+
+/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+
+const card2 = document.querySelector('.card2');
+const card2Photo = document.querySelector('.card2__photo');
+const cart2Heading = document.querySelector('.cart2__heading');
+const card2Description = document.querySelector('.card2__description');
+const card2Price = document.querySelector('.card2__price');
+const card2Btn1 = document.querySelector('.card2__btn-1');
+const card2Btn2 = document.querySelector('.card2__btn-2');
+const card2Logo = document.querySelector('.card2__logo');
+
+
+function card2Open() {
+    card2.classList.toggle('card2--open');
+    card2Photo.classList.toggle('card2__photo--open');
+    cart2Heading.classList.toggle('cart2__heading--open');
+    card2Description.classList.toggle('card2__description--open');
+    card2Price.classList.toggle('card2__price--open');
+    card2Btn1.classList.toggle('card2__btn-1--open');
+    card2Btn2.classList.toggle('card2__btn-2--open');
+    card2Logo.classList.toggle('card2__logo--open');
+
+}
+
+
+card2Photo.addEventListener('click', card2Open);
